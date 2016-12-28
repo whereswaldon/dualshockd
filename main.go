c91c1f7a38052de81ce4561c6d5d2b2285117910
Christopher Waldon <christopher.waldon.dev@gmail.com>
2016-12-28 09:56:03 -0500

    Implement memory profiling

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
+	"github.com/pkg/profile"
 	"github.com/whereswaldon/dualshockd/controllers"
+	"os"
+	"os/signal"
 )
 
 func main() {
+	defer profile.Start(profile.MemProfile).Stop()
 	done := make(chan struct{})
 	defer close(done)
 	m, err := controllers.NewMonitor(done)
@@ -13,7 +17,15 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+	interrupt := make(chan os.Signal)
+	signal.Notify(interrupt, os.Interrupt)
 	for c := range m.Controllers() {
-		fmt.Println(c)
+		select {
+		case <-interrupt:
+			fmt.Println("exiting")
+			return
+		default:
+			fmt.Println(c)
+		}
 	}
 }
